073b633e0bfe6bd729708e724ccd6a18e481a44d
d2r3v <hereisdhruv007@gmail.com>
2020-09-08 21:20:05 +0530

    Added primeFac

--- src/Question_1.java
+++ src/Question_1.java
@@ -2,6 +2,9 @@ public class Question_1 {
 
     public static void main(String[] Args) {
         System.out.println(Question_1.IsPrime(19));
+        for (int element: Question_1.primeFac(25,2,1)) {
+            System.out.println(element);
+        }
     }
 
     static boolean IsPrime(int n)
@@ -16,6 +19,22 @@ public class Question_1 {
             }
         }
 return prime;
-
     }
-}
+
+    static int [] primeFac(int c, int f,int p)
+    {
+
+        if (c == 1)
+        {
+            int a[] = {};
+            return a;
+        }
+        if (c % f == 0) {
+            int fac [] = primeFac(c/f, f,p++);
+            f= fac [p];
+            return fac;
+        } else
+            return primeFac(c, f+1,p++);
+    }
+    }
+
