7bc3c3048dab40f424aafc1d71413a2dc6994bfc
d2r3v <hereisdhruv007@gmail.com>
2020-09-08 20:38:36 +0530

    Added IsPrime

--- src/Question_1.java
+++ src/Question_1.java
@@ -1,10 +1,22 @@
-import java.util.*;
-import java.io.*;
 public class Question_1 {
 
-    public static void main (String [] Args)
-    {
-
+    public static void main(String[] Args) {
+        System.out.println(Question_1.IsPrime(19));
     }
 
+    static boolean IsPrime(int n)
+    {
+        boolean prime = true;
+        for (int i = 2; i <= n / 2; i++)
+        {
+            int a = n % i;
+            if (a == 0) {
+                prime = false;
+                return prime;
+            }
+        }
+return prime;
+
+
+    }
 }
