7645a9929e27ed90984cdc56e580cb01c4a33500
d2r3v <hereisdhruv007@gmail.com>
2020-09-14 12:48:08 +0530

    Final Commit

---
+++ src/Question_2.java
@@ -0,0 +1,10 @@
+public class Question_2 {
+
+    public static void main(String [] Args)
+    {
+         int [][]input = {{0,1,0,1,1},{1,0,1,0,1},{0,1,0,0,0,},{1,0,0,0,1},{1,1,0,1,0}};
+
+
+
+    }
+}
