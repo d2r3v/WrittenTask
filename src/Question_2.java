bfd9b17962af830cad1f772c72e19d4fab1c70b2
d2r3v <50176999+d2r3v@users.noreply.github.com>
2020-09-16 12:26:56 +0530

    Update Question_2.java

--- src/Question_2.java
+++ src/Question_2.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Question_2 {
     static int sum = 0;
-    static int sum1 = 0;
 
     public static void main(String[] Args) {
         Scanner sc= new Scanner(System.in);
@@ -49,7 +48,65 @@ for (int i = 0; i < 5; i++) {
             a[i][j]=b[i][j];
             b[i]=a[j++];
             }
+import java.util.*;
+class Shift {
+    static int mat[][];
+   static int m, n;
+
+    public static void main(String[] Args) {
+
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter number of rows ");
+        m=sc.nextInt();
+        System.out.println("Enter number of Columns ");
+        n=sc.nextInt();
+        Shift a = new Shift(m,n);
+        Shift b = new Shift(m,n);
+        a.input();
+        b.cyclic(a);
+        a.display();
+    }
+
+    Shift(int mm, int nn) {
+        n = nn;
+        m = mm;
+        mat = new int[m][n];
+    }
+
+    void cyclic(Shift P) {
+        for (int j = 0; j < 5; j++) {
+            P.mat[0][j] = mat[0][j];
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                mat[i][j] = P.mat[i][j];
+                P.mat[i] = mat[j++];
+            }
+        }
+    }
+
+    void display() {
+        for (int s = 0; s < m; s++) {
+            System.out.println("");
+            for (int q = 0; q < n; q++) {
+                System.out.print(mat[s][q] + " ");
+            }
+
+        }
+    }
+
+    void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the elements of the array ");
+        for (int s = 0; s < m; s++) {
+            for (int q = 0; q < n; q++) {
+                mat[s][q] = sc.nextInt();
+            }
+        }
+    }
+}
+
+
 
  */
-
 
