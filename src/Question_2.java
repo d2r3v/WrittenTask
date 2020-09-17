ef19cc1bb583d021e40ee4345fb74e8385c07332
d2r3v <50176999+d2r3v@users.noreply.github.com>
2020-09-17 12:10:22 +0530

    Final Commit

--- src/Question_2.java
+++ src/Question_2.java
@@ -13,6 +13,7 @@ public class Question_2 {
         int[][] input = {{0, 1, 0, 1, 1}, {1, 0, 1, 0, 1}, {0, 1, 0, 0, 0,}, {1, 0, 0, 0, 1}, {1, 1, 0, 1, 0}};
        System.out.println(Question_2.CommonFriends(input,a,b));
         obj2.Friends(input);
+        System.out.println("Numbers of pairs of friends");
         System.out.println(sum / 2);
 
     }
